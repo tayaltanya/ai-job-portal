5eb772965007343f96f21e5e194f9b7134f0f087
Tanya Tayal <tayaltanya66@gmail.com>
2026-06-03 12:40:05 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://ai-job-portal-eight.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json());
 
 // Routes
